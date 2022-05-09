ff5a38406753c368f037ce02ef775b110b3a5ff5
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2022-05-09 14:40:20 +0200

    Socket starter

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+
+send("Hello RSA")
+# pause on sending messages to test multiple clients
+input()
+send("Hello Mango!🥭")
+input()
+
+send(DISCONNECT_MESSAGE)
